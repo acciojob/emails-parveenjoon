f2e75c3fce1baa74a237a908699eafa41c26b134
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-03-02 18:24:09 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,8 +1,6 @@
-package com.driver;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalTime;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
@@ -14,20 +12,20 @@ public class Main {
         Gmail gmail = new Gmail("accio@gmail.com", 3);
         gmail.receiveMail(new SimpleDateFormat("dd/MM/yyyy").parse("21/12/2022"), "Tushar", "Assignment Completed?");
         gmail.receiveMail(new SimpleDateFormat("dd/MM/yyyy").parse("22/12/2022"), "Tushar", "We are running out of time.");
-        gmail.receiveMail(new SimpleDateFormat("dd/MM/yyyy").parse("22/12/2022"), "Abhishek", "Assignment to be uploaded on database.");
+        gmail.receiveMail(new SimpleDateFormat("dd/MM/yyyy").parse("22/12/2022"), "Abhishek", "Assignment to be uploaded on the database.");
         gmail.receiveMail(new SimpleDateFormat("dd/MM/yyyy").parse("23/12/2022"), "Tushar", "Everything looks good.");
 
         System.out.println("Total " + gmail.getInboxSize() + " mails in inbox.");
         System.out.println("There are " + gmail.findMailsBetweenDates(new SimpleDateFormat("dd/MM/yyyy").parse("21/12/2022"), new SimpleDateFormat("dd/MM/yyyy").parse("22/12/2022")) + " mails between given dates!");
         gmail.deleteMail("Everything looks good.");
-        System.out.println("The latest message is :" + gmail.findLatestMessage());
-        gmail.deleteMail("Assignment to be uploaded on database.");
+        System.out.println("The latest message is: " + gmail.findLatestMessage());
+        gmail.deleteMail("Assignment to be uploaded on the database.");
         System.out.println("There are " + gmail.getTrashSize() + " mails in the trash.");
         gmail.emptyTrash();
         System.out.println("There are " + gmail.getTrashSize() + " mails in the trash.");
 
         Workspace workspace = new Workspace("accio@gmail.com");
-        System.out.println("Inbox capacity is " + workspace.getInboxCapacity() + " in workspace.");
+        System.out.println("Inbox capacity is " + workspace.getInboxCapacity() + " in the workspace.");
 
         workspace.addMeeting(new Meeting(LocalTime.parse("17:40"), LocalTime.parse("18:40")));
         workspace.addMeeting(new Meeting(LocalTime.parse("13:30"), LocalTime.parse("18:00")));
